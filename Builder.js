f071012c14155f63b80f50845e1db40f5c169a1e
zhangjun <zhangjun@xiaoheiban.cn>
2020-12-07 15:16:18 +0800

    update:建造者模式

---
+++ Builder.js
@@ -0,0 +1,99 @@
+/**
+ * 建造者模式：
+ * 指将一个复杂对象的构造与它的表示分离，使同样的构建过程可以创建不同的表示.
+ * 建造者模式 是由 产品，抽象建造者，具体建造者，指挥者等4个要素构成
+ */
+class Product{
+  setCpu(cpu){
+    this.cpu = cpu
+  }
+  setDisk(disk){
+    this.disk = disk
+  }
+  setVideoCard(videoCard){
+    this.videoCard = videoCard
+  }
+  show(){
+    console.log(`您可以购买的产品属性为：cpu:${this.cpu}  硬盘：${this.disk}  显卡：${this.videoCard}`)
+  }
+}
+
+class Builder {
+  constructor() {
+    this.product = new Product()
+  }
+  buildCpu(){}
+  buildDisk(){}
+  buildVideoCard(){}
+  getResult(){
+    return this.product
+  }
+}
+
+class LowComputerBuild extends Builder{
+  buildCpu() {
+    super.buildCpu();
+    console.log('cpu: i3')
+    this.product.setCpu('i3')
+  }
+  buildDisk() {
+    super.buildDisk();
+    console.log('disk: 512G')
+    this.product.setDisk('512G')
+  }
+  buildVideoCard() {
+    super.buildVideoCard();
+    console.log('video card: GTX980')
+    this.product.setVideoCard('GTX980')
+  }
+}
+class GoodComputerBuild extends Builder{
+  buildCpu() {
+    super.buildCpu();
+    console.log('cpu: i9')
+    this.product.setDisk('i9')
+  }
+  buildDisk() {
+    super.buildDisk();
+    console.log('disk: 2T')
+    this.product.setDisk('2T')
+  }
+  buildVideoCard() {
+    super.buildVideoCard();
+    console.log('video card: GTX3070')
+    this.product.setVideoCard('GTX3070')
+  }
+}
+
+class Director{
+  constructor(money) {
+    console.log('电脑销售人员：你好，你的口袋里装了多少钱啊',`我：我口袋里面有${money}`)
+    if (money<10000){
+      console.log('电脑销售人员:穷鬼')
+      this.builder = new LowComputerBuild()
+    }else {
+      console.log('电脑销售人员:有钱人呐')
+      this.builder = new GoodComputerBuild()
+    }
+  }
+  construct(){
+    this.builder.buildCpu()
+    this.builder.buildDisk()
+    this.builder.buildVideoCard()
+    return this.builder.getResult()
+  }
+}
+
+function buyComputer() {
+  console.log('I have $1000')
+  let salesman = new Director(1000)
+  let product =  salesman.construct()
+  product.show()
+  setTimeout(() => {
+    console.log('after 2 years working ---> I have $10001 ')
+    salesman = new Director(10001)
+    product = salesman.construct()
+    product.show()
+  },2000)
+}
+buyComputer()
--- FactoryMethod.js
+++ FactoryMethod.js
@@ -2,3 +2,85 @@
  * 工厂方法：
  * 定义一个用于创建产品的接口，由子类决定生产什么产品。
  */
+/**
+ * 工厂方法模式
+ */
+class Computer {
+  createComputer(){}
+}
+class Cpu extends Computer{
+  createComputer() {
+    super.createComputer()
+    console.log('生产cpu')
+  }D
+}
+class Disk extends Computer{
+  createComputer() {
+    super.createComputer()
+    console.log('生产disk')
+  }
+}
+class ComputeFactory {
+  getProduct(){}
+}
+class CpuFactory extends ComputeFactory{
+  getProduct() {
+    super.getProduct();
+    return new Cpu()
+  }
+}
+class DiskFactory extends ComputeFactory{
+  getProduct() {
+    super.getProduct();
+    return new Disk()
+  }
+}
+
+function product() {
+  let cpuFactory = new CpuFactory()
+  cpuFactory.getProduct().createComputer()
+  console.log('===>')
+  let diskFactory = new DiskFactory()
+  diskFactory.getProduct().createComputer()
+}
+
+product()
+
+/**
+ * 简单工厂模式
+ */
+
+class Keyboard{
+  constructor(props) {
+     this.getProduct()
+  }
+
+  getProduct() {
+    console.log('生产键盘')
+  }
+}
+
+class Mouse {
+  constructor(props) {
+    this.getProduct()
+  }
+  getProduct ( ){
+    console.log('生产鼠标')
+  }
+}
+
+console.log('------------')
+function PeripheralFactory(product) {
+  let peripheralProduct = null
+  if (product === 'keyboard'){
+    peripheralProduct = new Keyboard()
+  }else if (product === 'mouse'){
+    peripheralProduct = new Mouse()
+  }else {
+    console.log('臣妾无能为力啊')
+  }
+  return peripheralProduct
+}
+PeripheralFactory('keyboard')
+PeripheralFactory('mouse')
+PeripheralFactory('screen')
